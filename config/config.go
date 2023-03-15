056905eded1ae9af8b6b5bca5f501e9f6ac9081f
VseZnal <Vseznal66@gmail.com>
2023-03-15 23:17:42 +0300

    chore: config

--- config/config.go
+++ config/config.go
@@ -1,1 +1,15 @@
 package config
+
+var (
+	tgApiToken = "TELEGRAM_APITOKEN"
+)
+
+type Config struct {
+	tgToken string
+}
+
+func BotConfig() *Config {
+	return &Config{
+		tgToken: tgApiToken,
+	}
+}
